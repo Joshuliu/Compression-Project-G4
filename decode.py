77459758363114c126ee88341526d79fceb5274a
lecoop <91681401+lecoop@users.noreply.github.com>
2021-10-17 13:33:00 -0400

    Finish encode.py

--- decode.py
+++ decode.py
@@ -1,24 +1,18 @@
-# import abstract syntax trees
 import ast
-# opens the file
-binaryfile = open("/Users/joshua/Documents/ECS 101 Project 1/binary_code.json", "r")
+import os
+
+directory = os.path.dirname(os.path.abspath(__file__))
+binaryfile = open(directory + "/binary_code.json", "r")
 
 #reading the file
-# converting the sting into binary
+# converting the string into dictionary
 contents = binaryfile.read()
 binary = ast.literal_eval(contents)
 #closing the file
 binaryfile.close()
 # prompting the user to put in the file code is using
-x = input('Enter file name to convert to text:')
+x = input('Enter file name to convert to text (should be in same folder as this script):')
 # reading the file user put in
-inputfile = open(x, "r").read()
+inputfile = open(directory + '/' + x, "r").read()
 
-# loop iterating over the string character by character, and adding the bianry value to the result
-for line in inputfile:
-result = ""
-for line in inputfile:
-    for character in line:
-        result = result + str(binary[character])
-#printin the result as string
-print(int(result))
+inputlist = [char for char in inputfile]
--- encode.py
+++ encode.py
@@ -1,14 +1,17 @@
 import ast
-binaryfile = open("/Users/joshua/Documents/ECS 101 Project 1/binary_code.json", "r")
+import os
+
+directory = os.path.dirname(os.path.abspath(__file__))
+binaryfile = open(directory + "/binary_code.json", "r")
 
 contents = binaryfile.read()
 binary = ast.literal_eval(contents)
 
 binaryfile.close()
 
-x = input('Enter file name to convert to binary: ')
+x = input('Enter file name to convert to binary (should be in same folder as this script): ')
 
-inputfile = open(x, "r").read()
+inputfile = open(directory + '/' + x, "r").read()
 
 inputlist = [char for char in inputfile]
 result = ""
@@ -16,18 +19,21 @@ num = 0
 
 while num < len(inputlist):
 
-    count = -1 # to determine how many characters to skip for multicharacter words 
+    count = -1
+    finalcount = -1
     test = ""
+    final = ""
     for x in range(5):
-        if num+x < len(inputlist) and test + inputlist[num + x] in binary.keys():
-                test += inputlist[num + x]
-                count += 1
+        if num+x < len(inputlist):
+            test += inputlist[num + x]
+            count += 1
+            if test + inputlist[num + x] in binary.keys():
+                final = test
+                finalcount = count
         else:
             break
 
-    num += 1 + count
-    print(test)
-    print(binary[test])
-    result += binary[test]
+    num += 1 + finalcount
+    result += binary[final]
 
 print(int(result))
